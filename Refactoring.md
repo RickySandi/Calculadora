f051e4450a565df8213884d901afd398b87e80c9
SebastianLazarte <72448046+SebastianLazarte@users.noreply.github.com>
2021-03-26 23:26:17 -0400

    Analizar 5to code smell

--- Refactoring.md
+++ Refactoring.md
@@ -1,28 +1,49 @@
-<br>**Code smell1**</br>
-<br>linea  codigos sucios</br>
-<br>Tipo de problema</br>
-<br>Como se refactorizo</br>
-<br>Linea de refactorizado</br>
-<br>**Code smell2**</br>
-<br>linea  codigos sucios</br>
-<br>Tipo de problema</br>
-<br>Como se refactorizo</br>
-<br>Linea de refactorizado</br>
-<br>**Code smell3**</br>
-<br>linea  codigos sucios</br>
-<br>Tipo de problema</br>
-<br>Como se refactorizo</br>
-<br>Linea de refactorizado</br>
-<br>**Code smell4**</br>
-<br>linea  codigos sucios</br>
-<br>Tipo de problema</br>
-<br>Como se refactorizo</br>
-<br>Linea de refactorizado</br>
-<br>**Code smell5**</br>
-<br>linea  codigos sucios</br>
-<br>Tipo de problema</br>
-<br>Como se refactorizo</br>
-<br>Linea de refactorizado</br>
-fasdf
+>**Code Smell 1**
 
-fasdaf
+>4-117
+
+>Metodo Grande
+
+>Dividiendo en varios metodos y creando una clase
+
+>2-20
+
+>**Code smell2**
+
+>46-53  60-67
+
+>Existe Codigo duplicado
+
+>Creando 2 funciones
+
+>25-35 37-48
+
+>**Code smell3**
+
+>7,20,44,53,69
+
+>Identificadores excesivamente cortos
+
+>Se realizo la asignacion de un nombre intuitivo y coherente
+
+>71,73,77,81,83
+
+>**Code smell4**
+
+>78-112
+
+>Codigo repetido
+>Se lo separo en un metodo
+
+>23-34 36-48
+
+>**Code smell5**
+
+>linea  codigos sucios
+
+>Tipo de problema
+
+>Como se refactorizo
+
+>Linea de refactorizado
+
--- RicardoSandi_Calculadora.py
+++ RicardoSandi_Calculadora.py
@@ -1,135 +1,91 @@
 import string
+class Calculadora:
+    def __init__(self, uno, dos):#tres
+        self.valor_1 = uno
+        self.valor_2 = dos
 
-def calcular(cantidad):
-    resultado=0
+    def suma_numeros(self):
+        return self.valor_1+self.valor_2
 
-    if cantidad == 2:
-      
-      x = input("Ingresa el primer valor: ")
-      x = float(x)
-      operacion =input("Ingresa el numero de la operacion a realizar 1. +, 2. -, 3. *, 4. / : ")
-      
-      if operacion == 1:
-        operacion = "+"
-      elif operacion == 2:
-        operacion = "-"
-      elif operacion == 3:
-        operacion = "*"
-      elif operacion == 4:
-        operacion ="/"
+    def resta_numeros(self):
+        return self.valor_1-self.valor_2
 
-      y = input("Ingresa el segundo valor: ")
-      y = float(y)
+    def multiplicacion_numeros(self):
+        return self.valor_1*self.valor_2
 
-      resultado = float(resultado)
+    def division_numeros(self):
+        try:
+             resultado=(self.valor_1/self.valor_2)
+        except Exception:
+            resultado="No se puede dividir con cero!\n"
+        return resultado
 
-      if x % x == 1 and y % y ==1:
-        x = int(x)
-        y = int(y)
-        resultado = int(resultado)
-      
-      if (operacion == "+"):
-        resultado = x +  y
-      elif operacion == "-":
-        resultado = x -  y 
-      elif operacion == "*":
-        resultado = x *  y 
-      else:
-        resultado = x / y 
+def operaciones_2_numeros(_1er_operador):
+      operaciones = { 
+    '+': calculadora.suma_numeros, 
+    '-': calculadora.resta_numeros, 
+    '*': calculadora.multiplicacion_numeros, 
+    '/': calculadora.division_numeros
+      }
+      try:
+            resultado=operaciones[_1er_operador]()
+      except Exception:
+            resultado="Operacion Invalida!\n"
+      return resultado
 
-    elif cantidad == 3:
-
-      x = input("Ingresa el primer valor: ")
-      x = float(x)
-
-      op1 =input("Ingresa el numero de la operacion a realizar 1. +, 2. -, 3. *, 4. / : ")
-
-      if op1 == 1:
-         op1 = "+"
-      elif op1 == 2:
-         op1 = "-"
-      elif op1 == 3:
-         op1 = "*"
-      elif op1 == 4:
-         op1 ="/"
-
-      y = input("Ingresa el segundo valor: ")
-      y = float(y)
-
-      op2 =input("Ingresa el numero de la operacion a realizar 1. +, 2. -, 3. *, 4. / : ")
-
-      if op2 == 1:
-         op2 = "+"
-      elif op2 == 2:
-        op2 = "-"
-      elif op2 == 3:
-        op2 = "*"
-      elif op2 == 4:
-        op2 ="/"
-
-      z = input("Ingresa el tercer valor: ")
-      z = float(z)
-
-      resultado = float(resultado)
-
-      if x % x == 1 and y % y ==1:
-        x = int(x)
-        y = int(y)
-        resultado = int(resultado)
-
-      if op1 == "+" and op2 == "+":
-        resultado = x + y + z
-      elif op1 == "+" and op2 == "-":
-        resultado = x +  y - z
-      elif op1 == "+" and op2 == "*":
-        resultado = x + y * z
-      elif op1 == "+" and op2 == "/":
-        resultado = x + y / z
-
-      elif op1 == "-" and op2 == "-":
-        resultado = x -  y - z
-      elif op1 == "-" and op2 == "+":
-        resultado = x -  y + z
-      elif op1 == "-" and op2 == "*":
-        resultado = (x - y) * z
-      elif op1 == "-" and op2 == "/":
-        resultado = (x - y) / z
-
-      elif op1 == "*" and op2 == "*":
-        resultado = x * y * z
-      elif op1 == "*" and op2 == "+":
-        resultado = (x * y) + z
-      elif op1 == "*" and op2 == "-":
-        resultado = (x * y) - z
-      elif op1 == "*" and op2 == "/":
-        resultado = (x * y) / z
-
-      elif op1 == "/" and op2 == "/":
-        resultado = (x / y) / z
-      elif op1 == "/" and op2 == "+":
-        resultado = (x / y) + z
-      elif op1 == "/" and op2 == "-":
-        resultado = (x / y) - z
-      elif op1 == '/' and op2 == "*":
-        resultado = (x / y) * z
-
-
+def operaciones_3_numeros(_1er_operador_2do_operador):
+    operaciones2 = { 
+    '+': calculadora2.suma_numeros, 
+    '-': calculadora2.resta_numeros, 
+    '*': calculadora2.multiplicacion_numeros, 
+    '/': calculadora2.division_numeros
+      }
+    try:
+        resultado=operaciones2[_2do_operador]()
+    except Exception:
+          resultado="Operacion Invalida!\n"
     return resultado
+ 
+print("\nEsta calculadora realiza las operaciones una a una. Si deseas calcular aoperaciones aritmeticas con tres numeros se dara prioridad a la operacion entre los dos valor_1s numeros elegidos y la primera operacion")
+print("A ese resultado se le sumara, restara, multiplicara o dividira, segun sea el caso, el tercer numero ingresado")
+print("Por ejemplo: Primer valor: 4  Primera operacion  : *  valor_2 valor: 2  Segunda operacion  : + Tercer valor: 3 \nResultado: (4 * 2) + 3 = 11 ")
+numero_de_valores=int(input("Cuantos valores desea incluir en el calculo 2 o 3 "))
 
-if __name__ == "__main__":
+if numero_de_valores==2:
+  valor_1=int(input("Inserta 1er Numero: "))
 
+  print("Operaciones a realizar\n1 - Sumar\n2 - Restar\n3 - Nultiplicar\n4 - Dividir\n")
+  
+  _1er_operador=input("Seleccione operacion: ")
+  
+  valor_2=int(input("Inserta 2do Numero: "))
+  calculadora=Calculadora(valor_1,valor_2)
+  
+  resultado_2_numeros=operaciones_2_numeros(_1er_operador)
+else:
+  valor_1=int(input("Inserta 1er Numero: "))
+  
+  print("Operaciones a realizar\n1 - Sumar\n2 - Restar\n3 - Nultiplicar\n4 - Dividir\n")
+  
+  _1er_operador=input("Seleccione operacion: ")
+  
+  valor_2=int(input("Inserta 2do Numero: "))
+  
+  calculadora=Calculadora(valor_1,valor_2)
+  
+  resultado_2_numeros=operaciones_2_numeros(_1er_operador)
+  
+  print("Operaciones a realizar\n1 - Sumar\n2 - Restar\n3 - Nultiplicar\n4 - Dividir\n")
+  
+  _2do_operador=input("Seleccione la segunda peracion: ")
+  
+  valor_3=int(input("Inserta 3er Numero: "))
+  
+  calculadora2=Calculadora(resultado_2_numeros,valor_3)
+  resultado_3_numeros=operaciones_3_numeros(_2do_operador)
+  
+if numero_de_valores==2:
+  print("El resultado es: ",resultado_2_numeros)
+else:
+  print("El resultado es: ",resultado_3_numeros)
 
-
- print("Esta calculadora realiza las operaciones una a una. Si deseas calcular aoperaciones aritmeticas con tres numeros se dara prioridad a la operacion entre los dos primeros numeros elegidos y la primera operacion")
- print("A ese resultado se le sumara, restara, multiplicara o dividira, segun sea el caso, el tercer numero ingresado")
- print("Por ejemplo: Primer valor: 4  Primera operacion  : *  Segundo valor: 2  Seunda operacion  : + Tercer valor: 3 Resultado: (4 * 2) + 3 = 11 ")
-
- print("Cuantos valores desea incluir en el calculo 2 o 3")
- cant = str(input())
- cantidad = int(cant)
-
- resultado = calcular(cantidad)
-
-
- print("Resultado: ", resultado)
-    
